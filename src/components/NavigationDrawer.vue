04dc5d91deae2319036b0e227d91e45094488322
istvanjuhasz <istvan.juhasz@telcotrend.hu>
2023-09-13 10:57:08 +0200

    set first tooltip in navigation drawer

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -1,30 +1,39 @@
 <template>
-    <v-card>
-      <v-layout>
-        <v-navigation-drawer v-model="drawer" :rail="rail" permanent>
-          <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="John Leider" nav @click="toggleRail">
-            <template v-slot:append>
-              <v-btn variant="text" icon="mdi-chevron-left" @click.stop="toggleRail"></v-btn>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer v-model="drawer" :rail="rail" permanent>
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="John Leider" nav
+          @click="toggleRail">
+          <template v-slot:append>
+            <v-btn variant="text" icon="mdi-chevron-left" @click.stop="toggleRail"></v-btn>
+          </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-tooltip text="Home" :open-on-hover="showTooltip">
+            <template v-slot:activator="{ props }">
+              <v-list-item prepend-icon="mdi-home-city" v-bind="props" title="Home" value="home" @click="goToHome">
+              </v-list-item>
             </template>
+          </v-tooltip>
+          <v-list-item prepend-icon="mdi-bed-double-outline " title="Hello World" value="home" @click="goToHelloWorld">
           </v-list-item>
-  
-          <v-divider></v-divider>
-  
-          <v-list density="compact" nav>
-            <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
-            <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
-            <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-        <v-main style="max-height: 850px"></v-main>
-      </v-layout>
-    </v-card>
-  </template>
+          <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="max-height: 850px"></v-main>
+    </v-layout>
+  </v-card>
+</template>
   
 
 <script setup lang="ts">
 import { ref, watch } from "vue";
 import { useDisplay } from 'vuetify'
+import router from "../router";
 // Destructure only the keys you want to use
 const { sm, xs, md, lg } = useDisplay();
 const isSm = ref(sm);
@@ -33,12 +42,27 @@ const isMd = ref(md)
 const isLg = ref(lg)
 const drawer = ref(true);
 const rail = ref(false);
+const showTooltip = ref(false)
+
 
 const toggleRail = () => {
   if (!(isSm.value || isXs.value || isMd.value || isLg.value)) {
     rail.value = !rail.value;
   }
+  if (rail.value) {
+    showTooltip.value = true
+  } else {
+    showTooltip.value = false
+  }
 };
+
+const goToHome = () => {
+  router.push('/')
+}
+const goToHelloWorld = () => {
+  router.push('/hello-world')
+}
+
 
 watch(isLg, (newVal) => {
   if (newVal) {
