4c4307f780208058aa3dabfd64271d1fe6c92634
istvanjuhasz <istvan.juhasz@telcotrend.hu>
2023-11-02 19:38:33 +0100

    base validation stucture

--- frontend/src/Views/RegisterForm.vue
+++ frontend/src/Views/RegisterForm.vue
@@ -1,12 +1,29 @@
 <template>
-    <div>
-        Regisztráció
-    </div>
-</template>
 
-<script setup lang="ts">
+    <v-row justify="center" >
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field
+          :rules="[emailRules.required, emailRules.regex]"
+          clearable
+          label="Email"
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
-</script>
+    <v-row justify="center" >
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field
+          :rules="[requiredRules.required]"
+          clearable
+          label="sima input"
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
-<style scoped>
-</style>
+  </template>
+  
+ 
+ <script setup lang="ts">
+ import { emailRules, requiredRules } from '../composables/validation/useValidation'
+ </script>
+ 
---
+++ frontend/src/composables/validation/useValidation.ts
@@ -0,0 +1,9 @@
+
+export const emailRules = {
+    required: (value: string) => !!value || 'A mező kitöltése kötelező!',
+    regex: (value: string) => /^[a-zA-Z0-9_.\-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]{2,4}$/.test(value) || 'Helytelen e-mail cím formátum!', 
+}
+
+export const requiredRules = {
+    required: (value: string) => !!value || 'A mező kitöltése kötelező!',
+}
