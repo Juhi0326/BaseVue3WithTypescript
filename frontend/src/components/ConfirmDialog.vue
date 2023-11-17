3edc3dbcc38e27fe7bdc1ab2c3c6285af9e6e7c4
istvanjuhasz <istvan.juhasz@telcotrend.hu>
2023-11-17 14:32:00 +0100

    create and use custom confirm dialog component

---
+++ frontend/src/components/ConfirmDialog.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-dialog v-model="dialog" :max-width="options.width" :style="{ zIndex: options.zIndex }" @keydown.esc="cancel">
+    <v-card>
+      <v-toolbar dark color="red" dense flat>
+        <v-toolbar-title>
+          {{ title }}
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-card-text v-show="!!message" class="pa-4" v-html="message"></v-card-text>
+      <v-card-actions class="pt-3">
+        <v-spacer></v-spacer>
+        <CustomButtonComponent class="ma-2" propColor="red" :propRounded="true" :propText="true" @click.native="cancel">
+          Mégsem
+        </CustomButtonComponent>
+
+        <CustomButtonComponent class="ma-2 my-3" propColor="red" :propRounded="true" :propDark="true"
+          @click.native="agree">
+          Igen
+        </CustomButtonComponent>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+
+import { ref, defineExpose } from 'vue'
+import CustomButtonComponent from '../components/CustomButtonComponent.vue'
+
+const resolve = ref<((value: boolean) => void) | null>(null);
+const reject = ref<((value: boolean) => void) | null>(null);
+
+const title = ref('')
+const message = ref('')
+const dialog = ref(false)
+const options = ref({
+  color: "red",
+  width: 400,
+  zIndex: 200,
+  noconfirm: false,
+})
+
+let agree = () => {
+  if (resolve.value) {
+    resolve.value(true);
+    dialog.value = false;
+  }
+};
+
+let cancel = () => {
+  if (reject.value) {
+    reject.value(false);
+    dialog.value = false;
+  }
+};
+
+
+const openDialog = (titleProp: string, messageProp: string) => {
+  title.value = titleProp;
+  message.value = messageProp;
+  dialog.value = true;
+  return new Promise<boolean>((resolve, reject) => {
+    agree = () => {
+      resolve(true);
+      dialog.value = false;
+    };
+
+    cancel = () => {
+      reject(false);
+      dialog.value = false;
+    };
+  });
+}
+
+
+const closeDialog = () => {
+  dialog.value = false
+}
+
+defineExpose({
+  openDialog,
+  closeDialog
+})
+
+
+
+</script>
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,67 +1,74 @@
 <template>
-    <v-layout>
-      <v-app-bar color="teal-darken-4" image="https://picsum.photos/1920/1080?random">
-        <template v-slot:image>
-          <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
-        </template>
+  <ConfirmDialog ref="cofirm"></ConfirmDialog>
+  <v-layout>
+    <v-app-bar color="teal-darken-4" image="https://picsum.photos/1920/1080?random">
+      <template v-slot:image>
+        <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
+      </template>
 
-        <v-app-bar-title class="ml-16">Juhi's Vue 3 project </v-app-bar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon v-if="!loggedIn" @click="logIn">mdi-door-closed-lock</v-icon>
-          <v-icon v-else @click="logOut">mdi-logout</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </v-app-bar>
-    </v-layout>
+      <v-app-bar-title class="ml-16">Juhi's Vue 3 project </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon v-if="!loggedIn" @click="logIn">mdi-door-closed-lock</v-icon>
+        <v-icon v-else @click="logOut('Kilépés', 'Biztosan ki szeretnél lépni?')
+          ">mdi-logout</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </v-layout>
   <NavigationDrawer />
 </template>
 
 <script setup lang="ts">
 import NavigationDrawer from './NavigationDrawer.vue';
 import { useAuthUserStore } from '../stores/user';
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import router from "../router";
 import { UseSnackBar } from '../stores/useSnackBar';
+import ConfirmDialog from '../components/ConfirmDialog.vue'
 
+const cofirm = ref<InstanceType<typeof ConfirmDialog> | null>(null)
 const useSnackBar = UseSnackBar();
 const authUserStore = useAuthUserStore();
 const loggedIn = computed(() => authUserStore.isLoggedIn)
-const logOut = () => {
-  try {
-    authUserStore.logOut()
-  openSnackbar()
-  } catch (error) {
-    logOutError()
+const logOut = async (titleProp: string, messageProp: string) => {
+  const result = cofirm.value && await cofirm.value.openDialog(titleProp, messageProp)
+  if (result) {
+    try {
+      authUserStore.logOut()
+      openSnackbar()
+    } catch (error) {
+      logOutError()
+    }
   }
-
+ 
 }
 const logIn = () => {
   router.push('/login')
 }
 
 const openSnackbar = () => {
-    useSnackBar.updateState(useSnackBar.$state, {
-        snackbar: {
-            visible: true,
-            text: 'Sikeres kijelentkezés!',
-            color: 'success',
-        }
-    })
+  useSnackBar.updateState(useSnackBar.$state, {
+    snackbar: {
+      visible: true,
+      text: 'Sikeres kijelentkezés!',
+      color: 'success',
+    }
+  })
 }
 
 const logOutError = () => {
-    useSnackBar.updateState(useSnackBar.$state, {
-        snackbar: {
-            visible: true,
-            text: 'Sikertelen kijelentkezés!',
-            color: 'error',
-        }
-    })
+  useSnackBar.updateState(useSnackBar.$state, {
+    snackbar: {
+      visible: true,
+      text: 'Sikertelen kijelentkezés!',
+      color: 'error',
+    }
+  })
 }
 </script>
